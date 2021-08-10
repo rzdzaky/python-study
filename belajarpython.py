9d2dbec9b22df7de13942b15781114cd24b6a3ee
rzdzaky <76608040+rzdzaky@users.noreply.github.com>
2021-08-10 19:13:03 +0700

    menambahakn program hitung keliling & luas lingkaran serta dapat mencari diameter berdasarkan luas lingkaran

--- belajarpython.py
+++ belajarpython.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 def main():
     os.system("cls")
@@ -7,7 +8,8 @@ def main():
         "1. Menghitung SPP",
         "2. CM ke Inchi Konverter",
         "3. Menghitung Luas Segitiga",
-        "4. Keluar"
+        "4. Menghitung Luas Lingkaran",
+        "5. Keluar"
     ]
     for i in menu:
         print(i)
@@ -16,7 +18,7 @@ def main():
 
     if (pilih==1):
         os.system("cls")
-        print("Program Menghitung SPP")
+        print("\nProgram Menghitung SPP")
         sarana = input("biaya sarana\t: ")
         spp_ttp = input("spp tetap\t: ")
         spp_var = input("spp var \t: ")
@@ -63,14 +65,79 @@ def main():
         os.system("cls")
         print("\nProgram Hitung Luas Segitiga")
 
-
     elif (pilih==4):
+        os.system("cls")
+        print("\nProgram Hitung Luas & Keliling Lingkaran")
+        hitung = [
+            "1. Luas",
+            "2. Keliling",
+            "3. Diameter berdasarkan luas"
+        ]
+        for i in hitung:
+            print(i)
+        pilih = input("Masukan Nomor Program yang Dipilih : ")
+        pilih = int(pilih)
+        
+        if (pilih==1):
+            os.system("cls")
+            print("\nMenghitung luas lingkaran")
+            r = input("Jari-jari\t: ")
+            r = float(r)
+            
+            L = r*r*3.14
+            
+            os.system("cls")
+            print("\nHasil perhitungan luas lingkaran")
+            L = f"Luas = phi * {r} x {r} \nLuas = {L}"
+            print(L)
+            input("\nTekan enter untuk lanjut..")
+            main()
+            
+        elif (pilih==2):
+            os.system("cls")
+            print("\nMenghitung keliling lingkaran")
+            d = input("Diameter\t: ")
+            d = float(d)
+            
+            K = d*3.14
+            
+            os.system("cls")
+            print("\nHasil perhitungan keliling lingkaran")
+            K = f"Keliling = phi x {d} \nKeliling = {K}"
+            print(K)
+            input("\nTekan enter untuk lanjut..")
+            main()
+            
+        elif (pilih==3):
+            os.system("cls")
+            print("\nMencari diameter lingkaran berdasar luas")
+            l = input("Luas\t: ")
+            l = int(l)
+            
+            d1 = l/3.14
+            D = math.sqrt(d1) * 2
+            
+            os.system("cls")
+            print("\nHasil perhitungan diameter lingkaran berdasar luas")
+            D = f"Diameter = {int(D)}"
+            print(D)
+            input("\nTekan enter untuk lanjut..")
+            main()
+            
+        else:
+            os.system("cls")
+            print("\nnomor program tidak tersedia !")
+            input()
+            main()
+            
+
+    elif (pilih==5):
         os.system("cls")
         print("\nAnda Keluar Program, Terimakasih Sudah Menggunakan")
         input()
         os.system("cls")
         exit()
-
+        
     else:
         os.system("cls")
         print("\nnomor program tidak tersedia !")
